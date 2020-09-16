789daa4c1eb5e19ba25e18a29714e74104899b77
Edyta Baluta <ebaluta@gmail.com>
2020-09-16 11:19:08 +0200

    Exercise2 done

--- function-events/my.js
+++ function-events/my.js
@@ -10,3 +10,20 @@ function check(){
         document.getElementById("result").innerHTML="Input is invalid. It's not a number";
     }
 }
+
+
+function exercise2(){
+    var num1 = document.getElementById("field1").value;
+    var num2=document.getElementById("field2").value;
+
+
+    var helper ="";
+        
+        for(var i =num1 ;i<=num2 ; i++){
+            helper = helper + i + " ";
+        }
+    
+    console.log(helper);
+
+    document.getElementById("exercise2").innerHTML=helper;
+}
